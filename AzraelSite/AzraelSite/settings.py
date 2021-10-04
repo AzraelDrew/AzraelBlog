35f84a80268bb153aa0c7b6445acce1d7b5079e5
Azrael <yznofsy@gmail.com>
2021-10-04 19:03:32 +0800

    update

--- AzraelSite/AzraelSite/settings.py
+++ AzraelSite/AzraelSite/settings.py
@@ -133,7 +133,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
     'http://127.0.0.1',
-    'http://localhost'
+    'http://localhost',
     'http://139.224.209.63'
 )
 
